5a6b495022ce7631f57dfd7d84f80b7c9213807b
Dan Lindsay <dlindsay72@hotmail.com>
2016-09-21 16:42:42 -0400

    Custom cell created, outlets connected

---
+++ ThePokedex/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  ThePokedex
+//
+//  Created by Dan Lindsay on 2016-09-21.
+//  Copyright © 2016 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
